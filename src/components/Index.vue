cd3a4cada87e68bbb07281b6829f8b1518dd23a1
rubialburqueque <rubi.vap@gmail.com>
2020-05-31 03:17:18 -0500

    avance template

---
+++ src/components/Index.vue
@@ -0,0 +1,26 @@
+
+<template>
+  <div class="home">
+    <Header/>
+    <Body/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+/* import Burger from '@/components/consultant/catalogue/Burger.vue'; */
+import Header from '@/components/Header.vue';
+import Body from '@/components/Body.vue';
+import Footer from '@/components/Footer.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    Header,
+    Body,
+    Footer,
+  },
+};
+</script>
+<style>
+</style>
